76579180c708b2f027c133367a3b72bf8dac4b75
Daniele Pagano <d.pagano@sportradar.com>
2020-05-19 11:18:31 +0200

    Removed tests to be done during the workshop

--- src/components/Panel/Panel.spec.js
+++ src/components/Panel/Panel.spec.js
@@ -1,10 +1,5 @@
-import React from "react";
-import { rendererWithTheme } from "../../utils/tests";
-import Panel from "./Panel";
-
 describe("Panel", () => {
-  it("should match the snapshot", () => {
-    const tree = rendererWithTheme(<Panel>My content</Panel>);
-    expect(tree).toMatchSnapshot();
+  it('succeeds', () => {
+    expect(true).toBe(true);
   });
 });
